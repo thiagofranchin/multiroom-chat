da8aecf086ac138e139b0c75f7966fc53f9405a1
Thiago Franchin <thiago.franchin@i-value.int>
2017-05-17 17:38:29 -0300

    Chat funcionando, listagem de participantes ok, scroll msg ok

--- app.js
+++ app.js
@@ -2,6 +2,59 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(80, function(){
+var server = app.listen(80, function(){
   console.log('Servidor online');
 })
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* Criar a conexão por websocket */ 
+io.on('connection', function(socket){
+  console.log('Usuário conectou');
+
+  socket.on('disconnect', function(){
+    console.log('Usuário desconectou');
+  });
+
+  socket.on('msgParaServidor', function(data){
+    //DIALOGO
+      // Retorna msg enviada apenas para quem enviou
+      socket.emit(
+        'msgParaCliente',
+        {
+          apelido: data.apelido, 
+          mensagem: data.mensagem
+        }
+      );
+      // Envia msg para todos os usuários menos para quem enviou
+      socket.broadcast.emit(
+        'msgParaCliente',
+        {
+          apelido: data.apelido, 
+          mensagem: data.mensagem
+        }
+      );
+    
+    // PARTICIPANTES
+      // Retorna msg enviada apenas para quem enviou
+      if(parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+          socket.emit(
+            'participantesParaCliente',
+            {
+              apelido: data.apelido
+            }
+          );
+          // Envia msg para todos os usuários menos para quem enviou
+          socket.broadcast.emit(
+            'participantesParaCliente',
+            {
+              apelido: data.apelido
+            }
+          );
+      }
+
+  });
+
+});
